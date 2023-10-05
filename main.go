701d4a0bd7b665850a6aeaf80a05c8d4d39a0dd2
Tony Reed <treed@dealerinspire.com>
2023-10-05 10:07:22 -0500

    add basic DB connection

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"net/http"
-
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
 )
 
 func main() {
+	// Setup DB connection
+	// TODO use os.Getenv and move the `export` statements to a DockerFile so it's automatic
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "password",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:33307",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+
+	// Get DB handle
+	var err error
+	db, err = sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	// Setup gin router
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	err := router.Run("localhost:8081")
-
+	err = router.Run("localhost:8081")
 	if err != nil {
-		panic("Something went wrong..")
+		log.Fatal(err)
 	}
 }
 
@@ -26,6 +51,8 @@ type album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
+
+var db *sql.DB
 
 // albums slice to seed record album data.
 var albums = []album{
